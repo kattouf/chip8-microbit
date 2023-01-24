22f300ab92b33acbdb7bde4573ccf385cb1cafb8
Kattouf <vasiliy.kattouf@gmail.com>
2023-01-24 22:09:17 +0700

    rewrite display impl to use direct driver compatible byte array data

--- src/peripheral/display.rs
+++ src/peripheral/display.rs
@@ -4,14 +4,13 @@ use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd13
 
 const BUFFER_WIDTH: usize = 64;
 const BUFFER_HEIGHT: usize = 32;
-
-static mut PIXEL_BUFFER: [bool; BUFFER_WIDTH * BUFFER_HEIGHT] =
-    [false; BUFFER_WIDTH * BUFFER_HEIGHT];
+const DISPLAY_WIDTH: usize = 128;
+const DISPLAY_HEIGHT: usize = 64;
 
 pub struct Display<I2C> {
     ssd1306driver:
         Ssd1306<I2CInterface<I2C>, DisplaySize128x64, BufferedGraphicsMode<DisplaySize128x64>>,
-    // pixel_buffer: [bool; 128 * 64],
+    pixel_buffer: [bool; BUFFER_WIDTH * BUFFER_HEIGHT],
 }
 
 impl<I2C> Display<I2C>
@@ -26,15 +25,13 @@ where
 
         Display {
             ssd1306driver: display,
-            // pixel_buffer: [false; 128 * 64],
+            pixel_buffer: [false; BUFFER_WIDTH * BUFFER_HEIGHT],
         }
     }
 
     pub fn clear_screen(&mut self) {
-        unsafe {
-            for val in PIXEL_BUFFER.iter_mut() {
-                *val = false;
-            }
+        for val in self.pixel_buffer.iter_mut() {
+            *val = false;
         }
         self.ssd1306driver.clear();
         self.ssd1306driver.flush().unwrap();
@@ -47,59 +44,59 @@ where
         bytes_len: usize,
     ) -> bool {
         let mut pixel_unset_flag = false;
-        unsafe {
-            for byte_num in 0..bytes_len {
-                if coordinate.1 + byte_num > BUFFER_HEIGHT - 1 {
-                    break;
-                }
-
-                let row_start = (coordinate.1 + byte_num) * BUFFER_WIDTH;
-                let row_end = (coordinate.1 + byte_num + 1) * BUFFER_WIDTH - 1;
-
-                let sprite_start: usize = row_start + coordinate.0 as usize;
-
-                let sprite_row_byte = data[byte_num as usize];
-                for offset in 0..8 {
-                    let sprite_bit = if (sprite_row_byte >> (7 - offset)) & 0b0000_0001 == 1 {
-                        true
-                    } else {
-                        false
-                    };
-                    let bit_location = sprite_start as usize + offset;
-                    if bit_location > row_end {
-                        break;
-                    }
-                    let prev_value = PIXEL_BUFFER[bit_location];
-                    let new_value = PIXEL_BUFFER[bit_location] ^ sprite_bit;
-                    PIXEL_BUFFER[bit_location] = new_value;
-                    pixel_unset_flag = pixel_unset_flag || prev_value == true && new_value == false;
-                }
+        for byte_num in 0..bytes_len {
+            if coordinate.1 + byte_num > BUFFER_HEIGHT - 1 {
+                break;
             }
 
-            let pixels = PIXEL_BUFFER.iter().enumerate().map({
-                |(offset, bit)| {
-                    Pixel(
-                        Point::new(
-                            (offset % BUFFER_WIDTH) as i32,
-                            (offset / BUFFER_WIDTH) as i32,
-                        ),
-                        BinaryColor::from(*bit),
-                    )
+            let row_start = (coordinate.1 + byte_num) * BUFFER_WIDTH;
+            let row_end = (coordinate.1 + byte_num + 1) * BUFFER_WIDTH - 1;
+
+            let sprite_start: usize = row_start + coordinate.0 as usize;
+
+            let sprite_row_byte = data[byte_num as usize];
+            for offset in 0..8 {
+                let sprite_bit = if (sprite_row_byte >> (7 - offset)) & 0b0000_0001 == 1 {
+                    true
+                } else {
+                    false
+                };
+                let bit_location = sprite_start as usize + offset;
+                if bit_location > row_end {
+                    break;
                 }
-            });
-            let upscale_pixels = pixels.flat_map({
-                |pixel| {
-                    [
-                        Pixel(Point::new(pixel.0.x * 2, pixel.0.y * 2), pixel.1),
-                        Pixel(Point::new(pixel.0.x * 2 + 1, pixel.0.y * 2), pixel.1),
-                        Pixel(Point::new(pixel.0.x * 2, pixel.0.y * 2 + 1), pixel.1),
-                        Pixel(Point::new(pixel.0.x * 2 + 1, pixel.0.y * 2 + 1), pixel.1),
-                    ]
-                }
-            });
-            self.ssd1306driver.draw_iter(upscale_pixels).unwrap();
-            self.ssd1306driver.flush().unwrap();
+                let prev_value = self.pixel_buffer[bit_location];
+                let new_value = self.pixel_buffer[bit_location] ^ sprite_bit;
+                self.pixel_buffer[bit_location] = new_value;
+                pixel_unset_flag = pixel_unset_flag || prev_value == true && new_value == false;
+            }
         }
+
+        let mut scaled_buffer = [false; DISPLAY_WIDTH * DISPLAY_HEIGHT];
+        let scale = DISPLAY_WIDTH / BUFFER_WIDTH;
+        for (offset, bit) in scaled_buffer.iter_mut().enumerate() {
+            let x = offset % DISPLAY_WIDTH;
+            let y = offset / DISPLAY_WIDTH;
+
+            let buf_x = x / scale;
+            let buf_y = y / scale;
+            let buf_offset = buf_y * BUFFER_WIDTH + buf_x;
+
+            *bit = self.pixel_buffer[buf_offset];
+        }
+
+        let mut raw_pixel_data = [0u8; DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
+        for (offset, byte) in raw_pixel_data.iter_mut().enumerate() {
+            let x = offset % DISPLAY_WIDTH;
+            let y_base = offset / DISPLAY_WIDTH * 8;
+            for y in y_base..(y_base + 8) {
+                let bit_loc = y * DISPLAY_WIDTH + x;
+                let value = if scaled_buffer[bit_loc] == true { 1 } else { 0 };
+                *byte = *byte & !(1 << (y - y_base)) | (value << (y - y_base))
+            }
+        }
+
+        self.ssd1306driver.draw(&raw_pixel_data).unwrap();
         pixel_unset_flag
     }
 }
